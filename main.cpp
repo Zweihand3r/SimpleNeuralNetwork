90dc801c2a5d208ee163835ba44b489d438789bf
zweihand3r <arnabbordoloi49@gmail.com>
2018-10-31 18:03:29 +0530

    Added charts

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 
 #include "cpp/filemanager.h"
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     qmlRegisterType<FileManager>("FileManager", 1, 1, "FileManager");
 
